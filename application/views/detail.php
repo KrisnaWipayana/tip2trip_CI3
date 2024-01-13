5e2138c6f39380a6c2657d217173b5aad8326fbf
Ramzhy <ramzhyputra12@gmail.com>
2024-01-13 16:21:01 +0800

    Benerin directory

--- application/views/detail.php
+++ application/views/detail.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
 
      <!-- my Css-->
-     <link href="detail.css" rel="stylesheet" >
+     <link href="assets/css/detail.css" rel="stylesheet" >
   </head>
   <body>
     
